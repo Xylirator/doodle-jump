82dac8275e2f85526bd52b4f120eac011e4a59d9
umar khan <umar.khan@student.uts.edu.au>
2022-11-29 20:33:45 +1100

    finished the game

---
+++ app.js
@@ -0,0 +1,195 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const doodler = document.createElement("div");
+  let isGameOver = false;
+  let platformCount = 5;
+  let platforms = [];
+  let score = 0;
+  let doodlerLeftSpace = 50;
+  let startPoint = 150;
+  let doodlerBottomSpace = startPoint;
+  let upTimerId;
+  let downTimerId;
+  let isJumping = true;
+  let isGoingLeft = false;
+  let isGoingRight = false;
+  let leftTimerId;
+  let rightTimerId;
+
+  class Platform {
+    constructor(newPlatformBottom) {
+      this.left = Math.random() * 315;
+      this.bottom = newPlatformBottom;
+      this.visual = document.createElement("div");
+
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+      grid.appendChild(visual);
+    }
+  }
+
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platformGap = 600 / platformCount;
+      let newPlatformBottom = 100 + i * platformGap;
+      let newPlatform = new Platform(newPlatformBottom);
+      platforms.push(newPlatform);
+      console.log(platforms);
+    }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+      platforms.forEach((platform) => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom + "px";
+
+        if (platform.bottom < 10) {
+          let firstPlatform = platforms[0].visual;
+          firstPlatform.classList.remove("platform");
+          platforms.shift();
+          console.log(platforms);
+          score++;
+          var newPlatform = new Platform(600);
+          platforms.push(newPlatform);
+        }
+      });
+    }
+  }
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add("doodler");
+    doodlerLeftSpace = platforms[0].left;
+    doodler.style.left = doodlerLeftSpace + "px";
+    doodler.style.bottom = doodlerBottomSpace + "px";
+  }
+
+  function fall() {
+    isJumping = false;
+    clearInterval(upTimerId);
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 5;
+      doodler.style.bottom = doodlerBottomSpace + "px";
+      if (doodlerBottomSpace <= 0) {
+        gameOver();
+      }
+      platforms.forEach((platform) => {
+        if (
+          doodlerBottomSpace >= platform.bottom &&
+          doodlerBottomSpace <= platform.bottom + 15 &&
+          doodlerLeftSpace + 60 >= platform.left &&
+          doodlerLeftSpace <= platform.left + 85 &&
+          !isJumping
+        ) {
+          console.log("tick");
+          startPoint = doodlerBottomSpace;
+          jump();
+          console.log("start", startPoint);
+          isJumping = true;
+        }
+      });
+    }, 20);
+  }
+
+  function jump() {
+    clearInterval(downTimerId);
+    isJumping = true;
+    upTimerId = setInterval(function () {
+      console.log(startPoint);
+      console.log("1", doodlerBottomSpace);
+      doodlerBottomSpace += 20;
+      doodler.style.bottom = doodlerBottomSpace + "px";
+      console.log("2", doodlerBottomSpace);
+      console.log("s", startPoint);
+      if (doodlerBottomSpace > startPoint + 200) {
+        fall();
+        isJumping = false;
+      }
+    }, 30);
+  }
+
+  function moveLeft() {
+    if (isGoingRight) {
+      clearInterval(rightTimerId);
+      isGoingRight = false;
+    }
+    if (isGoingLeft) {
+    } else {
+      isGoingLeft = true;
+      leftTimerId = setInterval(function () {
+        if (doodlerLeftSpace >= 0) {
+          doodlerLeftSpace -= 5;
+          doodler.style.left = doodlerLeftSpace + "px";
+        } else {
+          moveRight();
+        }
+      }, 30);
+    }
+  }
+
+  function moveRight() {
+    if (isGoingRight) {
+      clearInterval(leftTimerId);
+      isGoingLeft = false;
+    }
+    if (isGoingRight) {
+    } else {
+      isGoingRight = true;
+      rightTimerId = setInterval(function () {
+        if (doodlerLeftSpace <= 340) {
+          console.log("going right");
+          doodlerLeftSpace += 5;
+          doodler.style.left = doodlerLeftSpace + "px";
+        } else {
+          moveLeft();
+        }
+      }, 30);
+    }
+  }
+
+  function moveStraight() {
+    isGoingLeft = false;
+    isGoingRight = false;
+    clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
+  }
+
+  function gameOver() {
+    isGameOver = true;
+    while (grid.firstChild) {
+      console.log("remove");
+      grid.removeChild(grid.firstChild);
+    }
+    grid.innerHTML = score;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+    clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
+  }
+
+  function control(e) {
+    doodler.style.bottom = doodlerBottomSpace + "px";
+    if (e.key === "ArrowLeft") {
+      moveLeft();
+    } else if (e.key === "ArrowRight") {
+      moveRight();
+    } else if (e.key === "ArrowUp") {
+      moveStraight();
+    }
+  }
+
+  function start() {
+    if (!isGameOver) {
+      createPlatforms();
+      createDoodler();
+      setInterval(movePlatforms, 30);
+      jump();
+      document.addEventListener("keyup", control);
+    }
+  }
+  start();
+});
